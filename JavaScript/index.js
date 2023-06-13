9d9b836449ca8e66243ee6a91b99bc02da9a2a93
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-06-13 19:27:10 +0100

    Real data (moment js tz)

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,37 @@
 // Los Angeles
-let losAngelesDateElement = document.querySelector("#los_angeles .date");
-let losAngelesTimeElement = document.querySelector("#los_angeles .time");
+setInterval(function () {
+  let losAngelesDateElement = document.querySelector("#los_angeles .date");
+  let losAngelesTimeElement = document.querySelector("#los_angeles .time");
 
-losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = "01:02:23 <smal> Am </small>";
+  let losAngeles = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngeles.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngeles.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}, 1000);
+
+// Buenos Aires
+setInterval(function () {
+  let buenosairesDateElement = document.querySelector("#buenos_aires .date");
+  let buenosairesTimeElement = document.querySelector("#buenos_aires .time");
+
+  let buenosaires = moment().tz("America/Buenos_Aires");
+
+  buenosairesDateElement.innerHTML = buenosaires.format("MMMM Do YYYY");
+  buenosairesTimeElement.innerHTML = buenosaires.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}, 1000);
+
+// Bangkok
+setInterval(function () {
+  let bangkokDateElement = document.querySelector("#bangkok .date");
+  let bangkokTimeElement = document.querySelector("#bangkok .time");
+
+  let bangkok = moment().tz("Asia/Bangkok");
+  bangkokDateElement.innerHTML = bangkok.format("MMMM Do YYYY");
+  bangkokTimeElement.innerHTML = bangkok.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}, 1000);
